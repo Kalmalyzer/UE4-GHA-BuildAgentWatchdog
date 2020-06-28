e3e8d1c2d78fef8051e4ec051a2d0672547a97c7
Mikael Kalms <mikael@kalms.org>
2020-06-28 17:55:11 +0200

    Print top-level errors as json messages with 'error' severity
    This will make those errors appear in GCP's Error Reporting

--- function.go
+++ function.go
@@ -41,11 +41,31 @@ type Result struct {
 	StoppedInstances []OnDemandInstance `json:"stopped_instances"`
 }
 
+type LogMessage struct {
+	Message  string `json:"message"`
+	Severity string `json:"severity"`
+}
+
+func produceInternalServerError(w http.ResponseWriter, format string, params ...interface{}) {
+	w.WriteHeader(http.StatusInternalServerError)
+
+	logMessage := LogMessage{Message: fmt.Sprintf(format, params...), Severity: "error"}
+	jsonLogMessage, err := json.Marshal(logMessage)
+	if err != nil {
+		log.Printf("Error while marshalling log message to json: %v\n", logMessage)
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+
+		fmt.Fprint(w, string(jsonLogMessage))
+		fmt.Println(string(jsonLogMessage))
+	}
+}
+
 func RunWatchdog(w http.ResponseWriter, r *http.Request) {
 
 	// Any panics within the application will result in a HTTP 500 Internal Server Error response
 	// This handler ensures that:
-	// * The panic error + stacktrace is visible in GCP's Logging, with severity "ERROR"
+	// * The panic error + stacktrace is visible in GCP's Logging, with severity "error"
 	// * The error shows up in GCP's Error Reporting
 	// * The panic error + stacktrace is returned in the HTTP response body
 	defer func() {
@@ -57,22 +77,37 @@ func RunWatchdog(w http.ResponseWriter, r *http.Request) {
 	}()
 
 	if _, err := ioutil.ReadAll(r.Body); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "Error while discarding body: %v", err)
-		log.Printf("%+v\n", err)
+		produceInternalServerError(w, "Error while discarding body: %+v\n", err)
 		return
 	}
 
 	project := os.Getenv("GCP_PROJECT")
+	if project == "" {
+		produceInternalServerError(w, "Misconfigured function: GCP_PROJECT must be set")
+		return
+	}
+
 	zone := os.Getenv("GCE_ZONE")
+	if zone == "" {
+		produceInternalServerError(w, "Misconfigured function: GCE_ZONE must be set")
+		return
+	}
+
 	gitHubOrganization := os.Getenv("GITHUB_ORGANIZATION")
+	if gitHubOrganization == "" {
+		produceInternalServerError(w, "Misconfigured function: GITHUB_ORGANIZATION must be set")
+		return
+	}
+
 	gitHubRepository := os.Getenv("GITHUB_REPOSITORY")
+	if gitHubRepository == "" {
+		produceInternalServerError(w, "Misconfigured function: GITHUB_REPOSITORY must be set")
+		return
+	}
 
 	startedInstances, stoppedInstances, err := Process(ctx, computeService, httpClient, gitHubClient, project, zone, gitHubOrganization, gitHubRepository)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "Error during processing: %v", err)
-		log.Printf("%+v\n", err)
+		produceInternalServerError(w, "Error during processing: %+v\n", err)
 		return
 	}
 
@@ -85,9 +120,7 @@ func RunWatchdog(w http.ResponseWriter, r *http.Request) {
 	result := &Result{StartedInstances: startedInstances, StoppedInstances: stoppedInstances}
 
 	if err := json.NewEncoder(w).Encode(result); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "Error during result json encoding: %v", err)
-		log.Printf("%+v\n", err)
+		produceInternalServerError(w, "Error during result json encoding: %+v\n", err)
 		return
 	}
 }
